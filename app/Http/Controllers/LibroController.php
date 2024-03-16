2c097bc6bb0da0926b27195c79f8db54233373db
Sebastian Medina <sebasmedina2003@gmail.com>
2024-03-15 23:06:48 -0400

    add: Update, Delete books

--- app/Http/Controllers/LibroController.php
+++ app/Http/Controllers/LibroController.php
@@ -25,11 +25,38 @@ class LibroController extends Controller
         $libro->imagen = $request->imagen;
         $libro->genero = $request->genero;
         $libro->autores = $request->autores;
-        $libro->fecha_publicacion = new \DateTime($request->fecha_publicacion);
+        $libro->fecha_publicacion = new \DateTime(str_replace('/', '-', $request->fecha_publicacion));
         $libro->calificacion = $request->calificacion;
         $libro->save();
 
         // Redirigir a la página de éxito o mostrar un mensaje
         return response()->json(Libro::all());
+    }
+
+    public function destroy($id)
+    {
+        $libro = Libro::find($id);
+
+        if ($libro) {
+            $libro->delete();
+            return response()->json(['message'=> 'Eliminado con exito'], 200);
+        }
+
+        return response()->json(['message'=> 'No se ha encontrado el libro'],400);
+    }
+
+    public function update(Request $request, $id){
+        $libro = Libro::find($id);
+        if (!$libro) {
+            return response()->json(['message'=> 'Bood dont find'], 404);
+        }
+
+        // Recorrer todos los campos del request
+        foreach ($request->all() as $campo => $valor) {
+            $libro->$campo = $valor;
+        }
+
+        $libro->save();
+        return response()->json(['message'=> 'Book Updated'], 200);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/libros', [LibroController::class, "index"])->name("crud.get");
 Route::post('/libros', [LibroController::class, "store"])->name("crud.create");
+Route::get('/libros', [LibroController::class, "index"])->name("crud.get");
+Route::put('/libros/{id}', [LibroController::class,'update'])->name('crud.update');
+Route::delete('/libros/{id}', [LibroController::class, 'destroy'])->name('curd.delete');
